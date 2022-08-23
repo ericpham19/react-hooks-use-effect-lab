7ab9571248a3337f375667da6ec896b0f44b1019
Duy Pham <ericpham19@outlook.com>
2022-08-23 16:01:44 +1000

    completed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+  const timeOut = setTimeout(() => {
+    setTimeRemaining((time) => time - 1);
+  }, 1000);
+  
+  if (timeRemaining === 0){
+    setTimeRemaining(10);
+    onAnswered(false);
+  }
+    return () =>{
+      clearTimeout(timeOut);
+    }
+  },[timeRemaining, onAnswered])
+
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -24,7 +39,7 @@ function Question({ question, onAnswered }) {
           </button>
         );
       })}
-      <h5>{timeRemaining} seconds remaining</h5>
+      <h5> {timeRemaining} seconds remaining</h5>
     </>
   );
 }
